2b9e65370f7b534577616d514da4ae6086423b9d
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2024-03-26 11:18:54 -0700

    updates

--- controllers/tankRoutes.js
+++ controllers/tankRoutes.js
@@ -3,7 +3,34 @@ const router = express.Router();
 const Tank = require('../models/Tank');
 
 router.get("/",(req,res)=>{
-    res.send("all the tanks here!")
+   Tank.findAll().then(data=>{
+    res.json(data)
+   })
+})
+router.get("/salties",(req,res)=>{
+   Tank.findAll({
+    where:{
+        isSalt:true
+    }
+   }).then(data=>{
+    res.json(data)
+   })
+})
+
+router.get("/:id",(req,res)=>{
+   Tank.findByPk(req.params.id).then(data=>{
+    res.json(data)
+   })
+})
+
+router.post("/",(req,res)=>{
+   Tank.create({
+    name:req.body.name,
+    gallons:req.body.gallons,
+    isSalt:req.body.isSalt
+   }).then(data=>{
+    res.json(data)
+   })
 })
 
 module.exports = router;
